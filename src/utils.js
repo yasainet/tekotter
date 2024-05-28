df7d3577cccfc078b285aa09c766392acb9891b8
yasainet <takumi.mizoguchi@gmail.com>
2024-05-29 02:31:45 +0900

    リファクタ

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,33 @@ const saveUserSetting = (userId, cupSetting) => {
   const timestamp = new Date();
   sheet.appendRow([timestamp, userId, cupSetting]);
 };
+
+const sendQuickReplyMessage = (replyToken, text, items) => {
+  const message = {
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: text,
+        quickReply: { items: items }
+      }
+    ]
+  };
+  UrlFetchApp.fetch(LINE_MESSAGING_API_URL, {
+    headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}` },
+    method: 'post',
+    payload: JSON.stringify(message)
+  });
+};
+
+const sendTextMessage = (replyToken, text) => {
+  const message = {
+    replyToken: replyToken,
+    messages: [{ type: 'text', text: text }]
+  };
+  UrlFetchApp.fetch(LINE_MESSAGING_API_URL, {
+    headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}` },
+    method: 'post',
+    payload: JSON.stringify(message)
+  });
+};
